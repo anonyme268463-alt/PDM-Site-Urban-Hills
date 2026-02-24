165d1a2321b248b934cfd64477ba1417d69e2a5a
anonyme268463-alt <anonyme268463@gmail.com>
2026-02-25 00:07:04 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAI3qBm_8zltFCTyPyLFNLcY-aU9GI1itM",
+  authDomain: "pdm-urban-hills.firebaseapp.com",
+  projectId: "pdm-urban-hills",
+  storageBucket: "pdm-urban-hills.firebasestorage.app",
+  messagingSenderId: "426355558661",
+  appId: "1:426355558661:web:8364782043dd5ad2270f6c",
+  measurementId: "G-RGM2DWVFLT"
+};
