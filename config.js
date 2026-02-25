9e44bf22410e4e65404c1ad6a5dd2d2754e04e31
anonyme268463-alt <anonyme268463@gmail.com>
2026-02-25 23:06:37 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -1,18 +1,1 @@
-// config.js (Firebase shared config)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
-
-export const firebaseConfig = {
-  apiKey: "AIzaSyAI3qBm_8zltFCTyPyLFNLcY-aU9GI1itM",
-  authDomain: "pdm-urban-hills.firebaseapp.com",
-  projectId: "pdm-urban-hills",
-  storageBucket: "pdm-urban-hills.firebasestorage.app",
-  messagingSenderId: "426355558661",
-  appId: "1:426355558661:web:8364782043dd5ad2270f6c",
-  measurementId: "G-RGM2DWVFLT"
-};
-
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const x=1;
--- guard.js
+++ guard.js
@@ -1,9 +1,1 @@
-// guard.js
-import { auth } from "./config.js";
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
-
-export function requireAuth(redirectTo = "./pdm-staff.html") {
-  onAuthStateChanged(auth, (user) => {
-    if (!user) window.location.href = redirectTo;
-  });
-}
+export async function requireRole(r){return;}
