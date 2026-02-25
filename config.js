cfd2691feb7f225766aab00a9ae8e9bd9e88e9b2
anonyme268463-alt <anonyme268463@gmail.com>
2026-02-25 20:45:29 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
-const firebaseConfig = {
+// config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
+
+export const firebaseConfig = {
   apiKey: "AIzaSyAI3qBm_8zltFCTyPyLFNLcY-aU9GI1itM",
   authDomain: "pdm-urban-hills.firebaseapp.com",
   projectId: "pdm-urban-hills",
@@ -7,3 +12,7 @@ const firebaseConfig = {
   appId: "1:426355558661:web:8364782043dd5ad2270f6c",
   measurementId: "G-RGM2DWVFLT"
 };
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
